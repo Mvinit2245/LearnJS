f600de4c6b6d208cdc545f49df44cb205cf06de4
Mvinit2245 <vinitmorankar22@gmail.com>
2025-07-11 22:22:46 +0530

    JS learning

---
+++ ArrayEx.js
@@ -0,0 +1,4 @@
+let arr = ["Vinit" ,"Om" , "Lalit", "Darshan"]
+
+arr.forEach((name , index) => {console.log(` Index ${index} : ${name}`);
+});
---
+++ StringEX.js
@@ -0,0 +1,7 @@
+const names = ("     VinitMorankar     ")
+console.log(names);
+console.log(names.trim());
+
+
+
+console.log(Object.getOwnPropertyNames(String.prototype))
