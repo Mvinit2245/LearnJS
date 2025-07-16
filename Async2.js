27c11c2c2dfa50335197d13acac43e072cffbd2c
Mvinit2245 <vinitmorankar22@gmail.com>
2025-07-16 13:41:44 +0530

    Callback global execution context commit

---
+++ Async2.js
@@ -0,0 +1,9 @@
+console.log("Hello This is Example of Async JS")
+
+function callback(){
+    console.log("This will printed at the last ")
+}
+
+setTimeout(callback, 20);
+
+console.log("Hello Aimerz")
